62bd747bb0e61a2d908ab76223195b9aa83f168a
Shantanu(CometChat) <93969013+Shantanu-CometChat@users.noreply.github.com>
2024-04-17 15:40:36 +0530

    added appID

--- app/src/main/java/com/cometchat/javasampleapp/AppConstants.java
+++ app/src/main/java/com/cometchat/javasampleapp/AppConstants.java
@@ -2,10 +2,10 @@ package com.cometchat.javasampleapp;
 
 public class AppConstants {
 
-    public static final String APP_ID = "XXXXXXXXXXXXXXXXX";
+    public static final String APP_ID = "212570a35d379d83";
 
-    public static final String AUTH_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    public static final String AUTH_KEY = "29743b3879623b539c54beda350c8d67434d1987";
 
-    public static final String REGION = "XX";
+    public static final String REGION = "US";
 
 }
